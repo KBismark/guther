b09aab6269539ac7a927c241b5c8c8b6c54af69f
KBismark <bismarkkwabenayamoah@gmail.com>
2024-07-23 08:39:02 +0000

    Update

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 
-const { update } = require('guther/lib/update');
-const { get } = require('guther/lib/get');
-const { remove } = require('guther/lib/remove');
-const { set_db_path } = require('guther/lib/_global');
+const { update } = require('./lib/update');
+const { get } = require('./lib/get');
+const { remove } = require('./lib/remove');
+const { set_db_path } = require('./lib/_global');
 
  let db_path_set = false;
  let guther = {
     update:update, get:get,
-    insert:update, remove:remove,
+    insert: async({id,data})=>{
+      let ex= null;
+     try {
+      ex = await get({id});
+     } catch (error) {}
+     if(!ex){
+      return await update({id,data})
+     }
+     throw new Error('Not inserted');
+    }, 
+    remove:remove,
     set_dir(dir){
        if(!db_path_set&&typeof dir!=='string') return;
        set_db_path(dir);
